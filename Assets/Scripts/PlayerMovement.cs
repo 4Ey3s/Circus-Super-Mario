17e7b263a8a87137603037f88474290e002a20f0
RizarOne <120001673+RizarOne@users.noreply.github.com>
2022-12-18 18:33:47 +0200

    Working PlayerMovement script
    Fixed PlayerMovement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,6 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
-
-    public Animator animator;
     public Rigidbody2D rb2D;
 
     public Transform groundCheckPosition;
@@ -37,16 +35,13 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") != 0) 
         {
             transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1); 
-            animator.SetBool("Walk", true);
-        }
-        else
-        {
-            animator.SetBool("Walk", false);
+        
+        
+        
         }
         if (Input.GetButtonDown("Jump") && grounded == true)
         {
             rb2D.velocity = new Vector2(0, jumpForce);
-            animator.SetTrigger("Jump");
         }
     }
 }
