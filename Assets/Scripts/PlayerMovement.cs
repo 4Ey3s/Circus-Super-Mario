833cda03b28d71d62d61b766ea43517a7708790f
RizarOne <120001673+RizarOne@users.noreply.github.com>
2022-12-18 18:17:40 +0200

    PlayerMovement
    Added script to player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Animator animator;
+    public Rigidbody2D rb2D;
+
+    public Transform groundCheckPosition;
+    public float groundCheckRadius;
+    public LayerMask groundCheckLayer;
+    public bool grounded;
+
+    void Start()
+    {    
+
+    }
+
+    void Update()
+    {
+        if (Physics2D.OverlapCircle(groundCheckPosition.position, groundCheckRadius, groundCheckLayer))
+        {
+            grounded = true;
+
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0); 
+
+        if (Input.GetAxisRaw("Horizontal") != 0) 
+        {
+            transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1); 
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
+        }
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            rb2D.velocity = new Vector2(0, jumpForce);
+            animator.SetTrigger("Jump");
+        }
+    }
+}
