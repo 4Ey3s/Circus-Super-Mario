e88886ae07d7c4cfe9f77b77b54fec8264717dd9
RizarOne <120001673+RizarOne@users.noreply.github.com>
2022-12-18 18:18:15 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterControl : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
