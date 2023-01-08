20360eef176cd7f9ec00ead2b3a080eb76219f20
RizarOne <120001673+RizarOne@users.noreply.github.com>
2023-01-08 18:15:45 +0200

    MainMenu & BetterJump
    Fixed jump gravity and added placeholder mainmenu

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,18 +9,27 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce;
     public Rigidbody2D rb2D;
 
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
     public Transform groundCheckPosition;
     public float groundCheckRadius;
     public LayerMask groundCheckLayer;
     public bool grounded;
 
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+    }
     void Start()
-    {    
+    {
 
     }
 
     void Update()
     {
+
         if (Physics2D.OverlapCircle(groundCheckPosition.position, groundCheckRadius, groundCheckLayer))
         {
             grounded = true;
@@ -30,18 +40,38 @@ public class PlayerMovement : MonoBehaviour
             grounded = false;
         }
 
-        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0); 
+        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
 
-        if (Input.GetAxisRaw("Horizontal") != 0) 
+        if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1); 
-        
-        
-        
+            transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1);
+
+
+
         }
         if (Input.GetButtonDown("Jump") && grounded == true)
         {
             rb2D.velocity = new Vector2(0, jumpForce);
+        }
+        {
+            if (rb2D.velocity.y < 0)
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+            }
+            else if (rb2D.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Scene currentScene = SceneManager.GetActiveScene();
+                {
+
+                    SceneManager.LoadScene("MainMenu");
+                }
+            }
         }
     }
 }
