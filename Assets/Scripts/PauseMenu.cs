1369da57072fd42f492305dbb3e51cb879cdf55c
RizarOne <120001673+RizarOne@users.noreply.github.com>
2023-01-14 18:39:42 +0200

    Added pauseMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    [SerializeField] GameObject pauseMenu;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+
+            }
+            else
+            { PauseGame();
+            
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Quit");
+        
+    }
+}
